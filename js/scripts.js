52574120d4c7eab75a67f0d6810d63f4b64ae823
aboutandre <github@aboutandre.com>
2019-02-02 15:00:22 +0100

    Create method to append employee data

--- js/scripts.js
+++ js/scripts.js
@@ -2,25 +2,36 @@ $(document).ready(function () {
 
     class Employee {
         // constructor(picture) {
-            constructor(picture, firstName, lastName, email, city, state, gender: male) {
-            this.pictureId = picture;
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.email = email;
-            this.city = city;
-            this.state = state;
-            this.gender = gender;
-            this.employeeTemplate = employeeTemplate;
+        constructor(data, employeeNumber) {
+            this.pictureId = data.results[employeeNumber].picture.large;
+            this.firstName = data.results[employeeNumber].name.first;
+            this.lastName = data.results[employeeNumber].name.last;
+            this.email = data.results[employeeNumber].email;
+            this.city = data.results[employeeNumber].location.city;
+            this.state = data.results[employeeNumber].location.state;
+            this.gender = data.results[employeeNumber].gender;
+            this.employeeTemplate = '';
         }
-        createEmployee () {
-            // let employeeCard = '<div class="card">';
+        createEmployee() {
+            this.employeeTemplate = '<div class="card">';
             let employeePicture = `
-         <div class="card-img-container">
-            <img class="card-img" src="${this.pictureId}" alt="profile picture">
-        </div>
-        `
-            return employeePicture;
-            // return this.employeeTemplate = employeePicture;
+                <div class="card-img-container">
+                    <img class="card-img" src="${this.pictureId}" alt="profile picture">
+                </div>
+                `
+            this.employeeTemplate += employeePicture;
+            let employeeInfo = '<div class="card-info-container">';
+            let employeeInfoDetails = `
+            <h3 id="name" class="card-name cap">first last</h3>
+                <p class="card-text">email</p>
+            <p class="card-text cap">city, state</p>
+            `
+
+
+
+            this.employeeTemplate += '</div>';
+            // return employeePicture;
+            return this.employeeTemplate;
         }
         greet() {
             return `${this.pictureId} says hello.`;
@@ -43,19 +54,20 @@ $(document).ready(function () {
     //     </div>
     // `
     $.ajax({
-        url: 'https://randomuser.me/api/?results=12&?nat=us,de,fr,gb,br&?gender=male',
+        url: 'https://randomuser.me/api/?results=12&?nat=us,de,fr,gb,br',
         dataType: 'json',
-        success: function(data) {
+        success: function (data) {
             // console.log(data.results);
             // let employee1 = new Employee(data.results.picture.large);
             // $('#gallery').html(employee1.createEmployee());
             let thisData = data.results;
-
-            let emp1 = new Employee(data.results[0].picture.large);
-            console.log(emp1.greet());
+            console.log(thisData);
+            let emp1 = new Employee(data, 0);
+            // console.log(emp1.greet());
+            console.log(emp1);
             emp1.appendEmployee();
             // console.log(emp1.createEmployee());
-            console.log(data.results[0].picture.large);
+            // console.log(data.results, 0);
         }
     });
 

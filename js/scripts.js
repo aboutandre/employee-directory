d3c681b5008b9cac30d521b0f0b9bb2ef4c63ec0
Andre Lorch <andre.lorch@kuehne-nagel.com>
2019-02-04 14:21:38 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -57,68 +57,111 @@ const destroyModal = function (modal) {
 };
 
 /**
- * Function that will navigate inside the modal window to the previous element
+ * Function that will navigate inside the modal window to the previous employee
  */
 const modalPrev = function () {
     $(document).on('click', '#modal-prev', function () {
+        // The employee number is attached to the "id" of the modal container
         let employeeNumber = $(this).closest('.modal-container').attr('id');
-        // Conditional to check if we are already in the last item/employee
-        if (employeeNumber <= 0) {
-            return
+        employeeNumber = parseInt(employeeNumber);
+        let previousEmployee = employeeNumber - 1;
+        // We will look for previous employees as long as we dont hit the first employee on the directory
+        while (previousEmployee >= 0) {
+            // Conditional to check if the previous employee has been filtered
+            if ($(`[data-employee-number='${previousEmployee}']`).hasClass('filtered')) {
+                // If yes, then subtract one to the current employee number and run the loop again
+                previousEmployee--;
+            } else {
+                // If the previous employee has not been filtered
+                // Destroy the modal
+                destroyModal($(this));
+                // Create a new modal for this employee
+                createModal(previousEmployee);
+                // Break out of the loop
+                break;
+            }
         }
-        // If we are not we destroy the current modal
-        destroyModal($(this));
-        // We subtract the current employee number
-        employeeNumber = parseInt(employeeNumber) - 1;
-        createModal(employeeNumber);
     });
 };
-
+/**
+ * Function that will navigate inside the modal window to the next employee
+ */
 const modalNext = function () {
     $(document).on('click', '#modal-next', function () {
+        // The employee number is attached to the "id" of the modal container
         let employeeNumber = $(this).closest('.modal-container').attr('id');
-        if (employeeNumber >= totalEmployees - 1) {
-            return
+        employeeNumber = parseInt(employeeNumber);
+        let nextEmployee = employeeNumber + 1;
+        // We will look for previous employees as long as we dont hit the last employee on the directory
+        while (nextEmployee < totalEmployees) {
+            // Conditional to check if the next employee has been filtered
+            if ($(`[data-employee-number='${nextEmployee}']`).hasClass('filtered')) {
+                // If yes, then add one to the current employee number and run the loop again
+                nextEmployee++;
+            } else {
+                // If the next employee has not been filtered
+                // Destroy the modal
+                destroyModal($(this));
+                // Create a new modal for this employee
+                createModal(nextEmployee);
+                // Break out of the loop
+                break;
+            }
         }
-        employeeNumber = parseInt(employeeNumber) + 1;
-        destroyModal($(this));
-        createModal(employeeNumber);
     });
 };
-
+/**
+ * Function to close the modal
+ */
 const closeModal = function () {
-
+    // Listener to see if the "#modal-close-btn" has been clicked
     $(document).on('click', '#modal-close-btn', function () {
+        // If yes, destroy the current modal
         destroyModal($(this));
     });
-
+    // Listener to see if the "Escape" key has been pressed.
     $(document).keydown(function (e) {
         if (e.which === 27) {
+            // Doing so will destroy the modal
             $('.modal-container').remove();
         }
     });
 };
-
+/**
+ * When the document is ready we call all the functions we need
+ */
 $(document).ready(function () {
+    // Random User API call
     $.ajax({
+        // We request 12 users from the USA, Germany, France, Great Britain and Brazil
         url: 'https://randomuser.me/api/?results=12&nat=us,de,fr,gb,br',
+        // The data type that we want is JSON
         dataType: 'json',
         success: function (data) {
+            // Function to deal with the received data results
             useReturnData(data.results)
 
         }
     });
-
+    // When we receive the data we set multiple variables, create objects and run methods
     function useReturnData(data) {
+        // We set the global "employeeData" variable to the received data from the AJAX call
         employeeData = data;
+        // Its useful to know how many employees we receive.
+        // Currently its 12 and we could hardcode the value here.
+        // But doing like this makes the application more flexible
         totalEmployees = employeeData.length;
+        // We create a new Directory using the Directory class and pass the received server data to it
         let newDirectory = new Directory(data);
+        // We call the "generateDirectory()" method that will create all the employees with the received data
         newDirectory.generateDirectory();
+        // We create a new search element
         newSearch = new Search(data);
+        // And call the method on it to append it to the DOM
         newSearch.addSearch();
-        console.log(employeeData);
     };
 
+    // We call all the functions we need when the document is ready
     openModal();
     closeModal();
     modalPrev();
